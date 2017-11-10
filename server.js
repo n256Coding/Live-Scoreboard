c79666b71e6c56ecb1e8ff82b9fabd433c5afd74
n256Coding <nishan256@gmail.com>
2017-11-10 21:03:47 +0530

    Dev2 commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var server = require('http');
+var io = require('socket.io').listen(server);
 
 var eventsRouter = require('./Routers/viewEvents.router');
 
@@ -15,16 +16,26 @@ app.use('/app', express.static(__dirname+'/public'));
 app.use('/app/modules', express.static(__dirname+'/bower_components'));
 app.use('/events', eventsRouter);
 
-var listenOutput = app.listen(3000, function (err) {
+/*
+app.listen(3000, function (err) {
     if(err){
         console.log('There is error in creating connection');
     }else{
         console.log('Server is listening on port '+3000);
     }
 });
+*/
+
+server.listen(3000);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname+'/public/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('newevent', function (data) {
+        //Hello This is dev2 branch
+    });
 });
 
 module.exports = listenOutput;
